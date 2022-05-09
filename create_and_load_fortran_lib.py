fc825ceaedee5377633adf42e17c93fa02073b46
Thomas Titscher <thomas.titscher@bam.de>
2022-05-09 20:41:09 +0200

    Separate file names

--- create_and_load_fortran_lib.py
+++ create_and_load_fortran_lib.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 def write_code(msg="Greetings!"):
-    with open("temp.cpp", "w") as f:
+    with open("temp_f.cpp", "w") as f:
         f.write(
             f"""
 #include "Derived.h"
@@ -26,22 +26,20 @@ void Derived::foo() {{ hello_(); }}
 
 write_code()
 
-lib_path = "./my_tmp_lib.so"
+lib_path = "./my_tmp_f_lib.so"
 
 p = subprocess.run(
-    ["gcc","-shared", "-fpic" , "-c", "temp.cpp"],
+    ["gcc", "-shared", "-fpic", "-c", "temp_f.cpp"],
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
 )
-print(p)
 
 p = subprocess.run(
-    ["gcc", "-o", lib_path, "temp.o","-shared" , "-L.", "-lhello", "-lgfortran"],
+    ["gcc", "-o", lib_path, "temp.o", "-shared", "-L.", "-lhello", "-lgfortran"],
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
 )
-print(p.stderr)
 
 import my_module
 
-my_module.load_plugin("./my_tmp_lib.so").foo()
+my_module.load_plugin(lib_path).foo()
